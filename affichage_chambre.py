e39993388951abcd0a3fb82552b49691c2c8257b
ATNK16 <atnk1606@gmail.com>
2025-11-07 10:55:45 +0100

    Ajout des menus

--- affichage_chambre.py
+++ affichage_chambre.py
@@ -3,10 +3,13 @@ offre_standard_ch_d = ['3 personnes', 'TV', 'Lit double 80 * 80','Petit frigo', 
 offre_suite_royale = ['2 personnes', 'TV', 'Lit double 200 * 200','Petit frigo', 'Machine à café', 'Set de thé','Set de bain','3 serviettes de douches', 'Corbeille de fruits','Serveur dédié à la chambre', 'Pack de bienvenue'] #a faire le dernier
 offre_studio = ['3 personnes', 'TV', 'Lit double 140 * 120','Petit frigo', 'Machine à café', 'Set de thé','Set de bain','3 serviettes de douches','Canapé','Kitchenette']
 import os 
-# def main() : 
+def main() : 
+   main_menu()
     
 
+
 def main_menu ():
+    os.system('cls')
     print("--------------------------------------------------")
     print("     BIENVENUE DANS NOTRE APP DE RESERVATION      ")
     print("--------------------------------------------------")
@@ -16,58 +19,92 @@ def main_menu ():
     choix = int(input("Veuillez choisir une option : "))
     print("--------------------------------------------------")
     while (choix < 1 or choix > 2) :
+        os.system('cls')
+        print("--------------------------------------------------")
+        print("     BIENVENUE DANS NOTRE APP DE RESERVATION      ")
+        print("--------------------------------------------------")
+        print("     1.VOIR NOS TOUTES LES OFFRES")
+        print("     2.QUITTER")
+        print("--------------------------------------------------")
         print("L'option", choix," ne figure pas parmi les options")
         choix = int(input("Veuillez choisir une option : "))
         print("--------------------------------------------------")
-    return choix 
+    menu_offres(choix)
+
+
 def menu_offres(choix) : 
+    os.system('cls')
     if choix == 1 :
         os.system('cls')
         print("--------------------------------------------------")
         print("VOICI LES OFFRES DISPONIBLES DANS NOTRE HOTEL")
         print("--------------------------------------------------")
-        print("1.OFFRE STANDARD (CHAMBRE INDIVIDUELLE)")
-        print("2.OFFRE STANDARD+ (CHAMBRE DOUBLE)")
-        print("3.OFFRE SUITE ROYALE ")
-        print("4.OFFRE STUDIO")
-        print("5.RETOUR")
+        print("1.OFFRE STANDARD (CHAMBRE INDIVIDUELLE) | QUANTITÉ : 3")
+        print("2.OFFRE STANDARD+ (CHAMBRE DOUBLE) | QUANTITÉ : 2")
+        print("3.OFFRE SUITE ROYALE | QUANTITÉ : 3")
+        print("4.OFFRE STUDIO | QUANTITÉ : 3")
+        print("0.RETOUR")
         print("--------------------------------------------------")
         choix_offre = int(input("Veuillez choisir une offre : "))
         print("--------------------------------------------------")
-        while choix_offre < 1 or choix_offre > 5 :
+        while choix_offre < 0 or choix_offre > 4 :
+            os.system('cls')
+            print("--------------------------------------------------")
+            print("VOICI LES OFFRES DISPONIBLES DANS NOTRE HOTEL")
+            print("--------------------------------------------------")
+            print("1.OFFRE STANDARD (CHAMBRE INDIVIDUELLE) | QUANTITÉ : 3")
+            print("2.OFFRE STANDARD+ (CHAMBRE DOUBLE) | QUANTITÉ : 2")
+            print("3.OFFRE SUITE ROYALE | QUANTITÉ : 3")
+            print("4.OFFRE STUDIO | QUANTITÉ : 2")
+            print("0.RETOUR")
+            print("--------------------------------------------------")
             print("L'option", choix_offre," ne figure pas parmi les offres")
             choix_offre = int(input("Veuillez choisir une offre : "))
             print("--------------------------------------------------")
-        return choix_offre
+        affichage_offre(choix_offre)
+    
+
 def affichage_offre(choix_offre) : 
-    retour = 0
+    retour = 1
     if choix_offre == 1 :
-        while retour != 1 : 
+        while retour != 0 : 
             affichage_titre("STANDARD")
             affichage(offre_standard_ch_i)
-            retour = int(input("1.Retour")) 
-        if retour == 1 :
+            print("0.Retour")
+            print("--------------------------------------------------")
+            retour = int(input("Veuillez choisir une option : "))
+            print("--------------------------------------------------")
+        if retour == 0 :
             menu_offres(1)
     elif choix_offre == 2 : 
-        while retour != 1 : 
+        while retour != 0 : 
             affichage_titre("CHAMBRE DOUBLE")
             affichage(offre_standard_ch_i)
-            retour = int(input("1.Retour")) 
-        if retour == 1 :
+            print("0.Retour")
+            print("--------------------------------------------------")
+            retour = int(input("Veuillez choisir une option : ")) 
+            print("--------------------------------------------------")
+        if retour == 0 :
             menu_offres(1)
     elif choix_offre == 3 :
-        while retour != 1 : 
+        while retour != 0 : 
             affichage_titre("SUITE ROYALE")
             affichage(offre_suite_royale)
-            retour = int(input("1.Retour"))
-        if retour == 1 :
+            print("0.Retour")
+            print("--------------------------------------------------")
+            retour = int(input("Veuillez choisir une option : "))
+            print("--------------------------------------------------")
+        if retour == 0 :
             menu_offres(1)
     elif choix_offre == 4 : 
-        while retour != 1 :
+        while retour != 0 :
             affichage_titre("STUDIO")
             affichage(offre_studio)
-            retour = int(input("1.Retour"))
-        if retour == 1 :
+            print("0.Retour")
+            print("--------------------------------------------------")
+            retour = int(input("Veuillez choisir une option : "))
+            print("--------------------------------------------------")
+        if retour == 0 :
             menu_offres(1)
     else : 
         os.system('cls')
@@ -81,4 +118,7 @@ def affichage (offre) :
     for i in offre: 
         print(i)
     print("--------------------------------------------------") 
+
+
+main()
 
